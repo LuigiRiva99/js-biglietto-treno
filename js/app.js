b02259b5f6e752b98b83aa53bc7beabde8f5358f
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-04 17:18:34 +0100

    Fix: variables from numbers to strings, added comments

--- js/app.js
+++ js/app.js
@@ -7,32 +7,35 @@
 
 
 // età utente prompt
-const età= parseInt(prompt("Inserisci età"));
+const eta= parseInt(prompt("Inserisci eta")); //string
 
 // numero chilometri prompt
-const chilometri= parseInt(prompt("Inserisci numero Km")); 
+const chilometri= parseInt(prompt("Inserisci numero Km")); //string
 
 // prezzo biglietto in base ai chilometri
-const bigliettoBase= 0.21 * chilometri;
+const bigliettoBase= 0.21 * chilometri; //number
 
-// biglietto sconto minorenni
-const ScontoMinori= bigliettoBase * 0.2;
-// biglietto sconto anziani
-const ScontoAnziani= bigliettoBase * 0.4;
+// sconto minorenni del 20%
+const ScontoMinori= bigliettoBase * 0.2; //number
+// sconto anziani del 40%
+const ScontoAnziani= bigliettoBase * 0.4; //number
 
-// biglietto scontato
-const bigliettoMinori= bigliettoBase - ScontoMinori;
-const bigliettoAnziani= bigliettoBase - ScontoAnziani; 
+// calcolo biglietto scontato
+const bigliettoMinori= bigliettoBase - ScontoMinori; //number
+const bigliettoAnziani= bigliettoBase - ScontoAnziani;  //number
+
+// Conversione da numero a stringa
+String(bigliettoBase, bigliettoMinori, bigliettoAnziani); //string
 
 //prezzi finali max 2 decimali
-const bigliettoBaseFinale = bigliettoBase.toFixed(2);
-const bigliettoMinoriFinale = bigliettoMinori.toFixed(2);
-const bigliettoAnzianiFinale = bigliettoAnziani.toFixed(2);
+const bigliettoBaseFinale = bigliettoBase.toFixed(2); //number
+const bigliettoMinoriFinale = bigliettoMinori.toFixed(2); //number
+const bigliettoAnzianiFinale = bigliettoAnziani.toFixed(2); //number
 
 // output 
-if (età < 18) {
+if (eta < 18) {
     console.log(bigliettoMinoriFinale);
-} else if (età > 65){
+} else if (eta > 65){
     console.log(bigliettoAnzianiFinale);
 } else {
     console.log(bigliettoBaseFinale);
